29614b434609596e223484ef2a2748fba7c365bb
Phelix <phelixmbani@gmail.com>
2024-09-26 11:55:28 +0300

    function scope

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+//
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';  
+}  
+
+function overwriteBestCustomer(bestCustomer){
+    window.bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'none'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'me';
+}
+console.log(changeLeastFavoriteCustomer())
